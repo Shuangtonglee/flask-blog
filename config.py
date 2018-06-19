986a76bd355c753af18a6fc2a7b2ffd657ff8ffb
Shuangtonglee <shuangtong_lee@163.com>
2018-06-19 22:51:27 +0800

    change database name

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ class HerokuConfig(Config):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_POSTS_PER_PAGE = 6
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+        'sqlite:///' + os.path.join(basedir, 'data_test.sqlite')
 
 
 config = {
